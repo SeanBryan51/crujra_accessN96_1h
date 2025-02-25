cdaa70e5df0741e13495389734256990c87ac1f4
Sean Bryan <sean.bryan@anu.edu.au>
2025-02-25 13:44:33 +1100

    Add support for consecutive runs with restarts
    Note this does not update the `ncciy` namelist option or any of the met
    forcing input paths for consecutive years. Only the path to the restart
    file is updated to point to the most recent run.

--- run.sh
+++ run.sh
@@ -8,5 +8,15 @@
 #PBS -l storage=gdata/rp23+gdata/tm70
 #PBS -j oe
 
-./setup.sh
-./cable > logs/cable_log_out.txt 2>&1
+NRUNS_DEFAULT=1
+
+set -e
+
+for i in $(seq ${NRUNS:=${NRUNS_DEFAULT}}); do
+    ./clean.sh
+    ./setup.sh
+    ./cable > logs/cable_log_out.txt 2>&1
+    output_dir=$(./save.sh)
+    # Point the RESTART environment variable to the previous restart directory for the next run
+    export RESTART=$(pwd)/${output_dir}/restart
+done

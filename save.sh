230d109724e2e6b2dfcfcd6b61165584e8e8fbb7
Sean Bryan <sean.bryan@anu.edu.au>
2025-02-25 13:44:33 +1100

    Try add support for running multiple times with restart files
    This still requires some work - we still need to find a way of updating
    `ncciy` or is it overwritten by a restart file?

---
+++ save.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+to_save=(outputs restart input logs cable_log_*)
+
+for path in ${to_save[@]}; do
+    if [ ! -f ${path} ] && [ ! -d ${path} ]; then
+        echo "Error: ${path} does not exist" >&2
+        exit 1
+    fi
+done
+
+archive_dir=./archive
+mkdir -p ${archive_dir}
+
+# See https://stackoverflow.com/questions/11307257/is-there-a-bash-command-which-counts-files
+output_count=$(ls 2>/dev/null -Ubad1 -- ${archive_dir}/* | wc -l)
+output_dir_name="${archive_dir}/output$(printf "%03d" ${output_count})"
+mkdir ${output_dir_name}
+
+for path in ${to_save[@]}; do
+    mv ${path} ${output_dir_name}
+done
+
+# Echo the output directory for the caller on success
+echo ${output_dir_name}

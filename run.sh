e55858e7e78cf87e6df88e2e6b73f813f5608c6b
Sean Bryan <sean.bryan@anu.edu.au>
2025-02-25 13:44:33 +1100

    Add support for consecutive runs with restarts
    Note this does not update the `ncciy` namelist option or any of the met
    forcing input paths for consecutive years. Only the path to the restart
    file is updated to point to the most recent run.

--- run.sh
+++ run.sh
@@ -9,19 +9,27 @@
 #PBS -j oe
 
 NCPUS_DEFAULT=4
+NRUNS_DEFAULT=1
 
-./setup.sh
+set -e
 
-if hostname -f | grep gadi.nci.org.au > /dev/null; then
+host_gadi() {
     libmpi_path_segments=($(ldd cable-mpi | awk '/=>/{print $(NF-1)}' | sed 's/\// /g' | grep libmpi.so))
     mpi_modulefile="${libmpi_path_segments[1]}/${libmpi_path_segments[2]}" # module_name/module_version
     if ! module is-avail ${mpi_modulefile}; then
         echo -e "Error: executable compiled with unknown MPI implementation."
         exit 1
     fi
-    echo "Loading module: ${mpi_modulefile}"
     module load ${mpi_modulefile}
     NCPUS=${PBS_NCPUS}
-fi
+}
 
-mpiexec -n ${NCPUS:=${NCPUS_DEFAULT}} ./cable-mpi > logs/cable_log_out.txt 2>&1
+for i in $(seq ${NRUNS:=${NRUNS_DEFAULT}}); do
+    ./clean.sh
+    ./setup.sh
+    if hostname -f | grep gadi.nci.org.au > /dev/null; then host_gadi; fi
+    mpiexec -n ${NCPUS:=${NCPUS_DEFAULT}} ./cable-mpi > logs/cable_log_out.txt 2>&1
+    output_dir=$(./save.sh)
+    # Point the RESTART environment variable to the previous restart directory for the next run
+    export RESTART=$(pwd)/${output_dir}/restart
+done

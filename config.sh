34c4737c6063dfc41c3bed385033c27c581dfbba
Sean Bryan <sean.bryan@anu.edu.au>
2025-02-26 11:36:42 +1100

    Try enabling CASA-CNP

--- config.sh
+++ config.sh
@@ -9,6 +9,8 @@ INPUT=(
     '/g/data/rp23/data/no_provenance/met_forcing/crujra_accessN96_1h/Wind/*'
     '/g/data/rp23/data/no_provenance/gridinfo/crujra_accessN96_gridinfo.nc'
     '/g/data/rp23/data/no_provenance/landmask/crujra_accessN96_landmask.nc'
+    '/g/data/rp23/data/modis_phenology_csiro.txt'
+    '/g/data/rp23/data/no_provenance/casa/pftlookup.csv'
     '/g/data/tm70/sb8430/crujra_accessN96_reduced_landmask/crujra_accessN96_reduced_landmask.nc'
 )
 
